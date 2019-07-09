3b7ccdc040660170e8c2b8d395b7d555d6af7e0d
Preston Elliott <preston.elliott24@gmail.com>
2019-07-09 12:29:53 -0400

    fixed migration file

--- db/migrate/20190709133222_drop_user_table.rb
+++
@@ -1,7 +0,0 @@
-class DropUserTable < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :email, :string
-    remove_column :users, :phone_number, :string
-    remove_column :users, :address, :string
-  end
-end
